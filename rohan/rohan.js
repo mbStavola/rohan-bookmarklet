7f0d5553bebf6924f1b901482f21b2f843aef7aa
mbs <mstavola9@gmail.com>
2014-07-09 03:07:11 -0400

    init

---
+++ rohan/rohan.js
@@ -0,0 +1,25 @@
+var numberOfPages = document.querySelector("td.specialtext:nth-child(2) > a:nth-child(6)").href.match(/page=\d+/)[0].match(/\d+/)[0];
+
+var randomPage = Math.floor(Math.random() * numberOfPages) + 1;
+
+var iframe = document.createElement("IFRAME");
+iframe.style.display = "none";
+document.body.appendChild(iframe);
+iframe.src = getRandomPage();
+
+var inter = window.setInterval(function() {
+    if (iframe.contentWindow.document.readyState === "complete") {
+      window.clearInterval(inter);
+      getRandomManga();
+    }
+}, 100);
+
+function getRandomPage(){
+    return window.location.href.replace(/page=\d+/, "page=" + randomPage).replace(/perpage=\d+/, "perpage=100");
+}
+
+function getRandomManga(){
+    var links = iframe.contentWindow.document.querySelectorAll("a[alt='Series Info']");
+    console.log("h");
+    window.location.href = links[Math.floor((Math.random() * links.length))];
+}
