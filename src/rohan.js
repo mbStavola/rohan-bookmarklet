302738bc518af8449b0581cfb49a6635410ad64e
mbStavola <Mstavola9@gmail.com>
2015-01-28 00:07:56 -0500

    Should fix browser issues

--- src/rohan.js
+++ src/rohan.js
@@ -1,24 +1,35 @@
 var numberOfPages = document.querySelector("td.specialtext:nth-child(2)").childNodes[0].nodeValue.match(/Pages \(\d+\)/)[0].match(/\d+/)[0];
 var randomPage = Math.floor(Math.random() * numberOfPages) + 1;
 
-var iframe = document.createElement("IFRAME");
-iframe.style.display = "none";
-document.body.appendChild(iframe);
-iframe.src = getRandomPage();
+var parser = new DOMParser()
+var doc = parser.parseFromString(getRandomPage(), "text/xml");
 
-var inter = window.setInterval(function() {
-    if (iframe.contentWindow.document.readyState === "complete") {
-      window.clearInterval(inter);
-      getRandomManga();
-    }
-}, 100);
+getRandomManga(doc);
 
 function getRandomPage(){
-    return window.location.href.replace(/page=\d+/, "page=" + randomPage).replace(/perpage=\d+/, "perpage=100");
+    return httpGet(window.location.href.replace(/page=\d+/, "page=" + randomPage).replace(/perpage=\d+/, "perpage=100"));
 }
 
-function getRandomManga(){
-    var links = iframe.contentWindow.document.querySelectorAll("a[alt='Series Info']");
+function getRandomManga(doc){
+    var links = doc.querySelectorAll("a[alt='Series Info']");
     console.log(links);
     window.location.href = links[Math.floor((Math.random() * links.length))];
+}
+
+function httpGet(theUrl){
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else {
+        // code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false);
+    xmlhttp.send();
 }
