932dd971ec6e1faba1303fb5a3202f89f47eaaf9
mbStavola <mbStavola@users.noreply.github.com>
2015-12-16 19:26:27 -0500

    Update rohan.js

--- src/rohan.js
+++ src/rohan.js
@@ -4,9 +4,6 @@ var jqScript = document.createElement("script");
 jqScript.src = "//code.jquery.com/jquery-1.11.3.min.js";
 document.head.appendChild(jqScript);
 
-var numberOfPages = document.querySelector("td.specialtext:nth-child(2)").childNodes[0].nodeValue.match(/Pages \(\d+\)/)[0].match(/\d+/)[0];
-var randomPage = Math.floor(Math.random() * numberOfPages) + 1;
-
 $.ajax({
     url : getRandomPage(),
     success : function(result){
@@ -17,13 +14,16 @@ $.ajax({
 function getRandomPage() {
     var resultURL = window.location.href;
 
+    var numberOfPages = document.querySelector("td.specialtext:nth-child(2)").childNodes[0].nodeValue.match(/Pages \(\d+\)/)[0].match(/\d+/)[0];
+    var randomPage = Math.floor(Math.random() * numberOfPages) + 1;
+
     if(resultURL.indexOf("?page=") > -1 || resultURL.indexOf("&page=") > -1 ) {
         resultURL = resultURL.replace(/page=\d+/, "page=" + randomPage);
     } else {
         resultURL = resultURL + "&page=" + randomPage;
     }
 
-    return httpGet(resultURL);
+    return resultURL;
 }
 
 function getRandomManga(doc) {
