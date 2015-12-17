5a3f8479277bd612bfc05eb72c6d2c883431f7d5
mbStavola <mbStavola@users.noreply.github.com>
2015-12-16 19:20:34 -0500

    Update rohan.js

--- src/rohan.js
+++ src/rohan.js
@@ -1,11 +1,18 @@
 debugger;
+
+var jqScript = document.createElement("script");
+jqScript.src = "//code.jquery.com/jquery-1.11.3.min.js";
+document.head.appendChild(jqScript);
+
 var numberOfPages = document.querySelector("td.specialtext:nth-child(2)").childNodes[0].nodeValue.match(/Pages \(\d+\)/)[0].match(/\d+/)[0];
 var randomPage = Math.floor(Math.random() * numberOfPages) + 1;
 
-var parser = new DOMParser();
-var doc = parser.parseFromString(getRandomPage(), "text/xml");
-
-getRandomManga(doc);
+$.ajax({
+    url : getRandomPage(),
+    success : function(result){
+        getRandomManga(result);
+    }
+});
 
 function getRandomPage() {
     var resultURL = window.location.href;
@@ -22,23 +29,4 @@ function getRandomPage() {
 function getRandomManga(doc) {
     var links = doc.querySelectorAll("a[alt='Series Info']");
     window.location.href = links[Math.floor((Math.random() * links.length))];
-}
-
-function httpGet(url) {
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            return xmlhttp.responseText;
-        }
-    }
-
-    xmlhttp.open("GET", url, false);
-    xmlhttp.send();
 }
